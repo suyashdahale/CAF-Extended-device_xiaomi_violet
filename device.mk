511bc7b3f3b0414061cc3e76a2c40820cb2ec83d
MLZ94 <zugutoroberto@gmail.com>
2021-05-17 09:35:10 +0000

    violet: added GCam
    ref:https://www.celsoazevedo.com/files/android/google-camera/dev-Nikita/f/dl2
    
    Signed-off-by: Md Abu Talha <abu.talha.asif.31@gmail.com>

--- device.mk
+++ device.mk
@@ -464,3 +464,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
+#GCAM
+PRODUCT_PACKAGES += \
+    GoogleCamera
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/GCAM/system/etc/permissions/com.google.android.GoogleCameraEng.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.google.android.GoogleCameraEng.xml
